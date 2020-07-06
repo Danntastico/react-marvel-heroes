8ddfbcc2eadf29ec08e803671c19f00518ab6225
Danilo Peña <nicolasdpf@gmail.com>
2020-07-05 21:38:05 -0500

    Container Component to MarvelHeroes

--- src/containers/MarvelHeroes/MarvelHeroes.jsx
+++ src/containers/MarvelHeroes/MarvelHeroes.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SearchHero } from '../../components/searchHero/SearchHero';
+import { getHeroesByMatch } from '../../helpers/SuperHeroAPI';
+import { useSearchResponse } from '../../hooks/useSearchResponse';
+import { CharacterCard } from '../../components/CharacterCard/CharacterCard';
+import './styles.css';
 
 export const MarvelHeroes = () => {
   const [heroByName, setHeroByName] = useState('');
-
-  console.log(heroByName);
+  const { data, loading } = useSearchResponse(heroByName, 10);
   return (
     <>
       <h1>Marvel</h1>
       <SearchHero searchHero={setHeroByName} />
+      <div className='search'>
+        <div className='search__results'>
+          {data.map((item) => (
+            <CharacterCard
+              key={item.id}
+              imageVariant='standard_xlarge'
+              {...item}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
