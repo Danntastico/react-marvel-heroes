26bcdb566efa2d02772f6960d6958d97114b5ff5
Danilo Peña <nicolasdpf@gmail.com>
2020-07-14 19:10:01 -0500

    dispatching login

--- src/containers/Login/LoginScreen.jsx
+++ src/containers/Login/LoginScreen.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
+  const { dispatch } = useContext(AuthContext);
   const handleClick = () => {
+    dispatch({
+      type: types.login,
+      payload: {
+        name: 'danilo',
+      },
+    });
     history.replace('/');
   };
   return (
